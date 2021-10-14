fdf26e8838374b67598b6092a9fb789a8f78d2e0
Sarah <sarahrudy@gmail.com>
2021-10-14 17:50:26 -0600

    Use 'movie' as a prop

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './MovieDetails.css'
 
-const MovieDetails = ({ title, backdrop_path, release_date }) => {
+const MovieDetails = ({ movie }) => {
+  console.log(movie)
   return (
-    <article className="movie-details">
-    <h3>{ title }</h3>
-    <img src={ backdrop_path } />
-    <p>{ release_date }</p>
+    <article className="movie-details" id={movie[0].id}>
+    <h3>{movie[0].title}</h3>
+      <img src={movie[0].backdrop_path} alt=''/>
+      <p>{movie[0].release_date}</p>
     </article>
   )
 }
