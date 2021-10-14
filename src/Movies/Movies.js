bec10ccaf46f1afb1f12f44da24655e141278eae
Sarah <sarahrudy@gmail.com>
2021-10-14 15:51:23 -0600

    Add movies as prop with map to display individual movie from data

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,10 +1,23 @@
-// import React from 'react'
-// import './Movies.css' 
-// import { movieData } from './movieData.js'
-
-// const Movies = () => {
-//   <div>Hi!</div>
-// }
+import React from 'react'
+import './Movies.css' 
+import MovieCard from '../MovieCard/MovieCard'
+import { movieData } from '../movieData'
 
 
-// export default Movies
+const Movies = ({ movies }) => {
+  const movieCard = movies.map(movie => {
+    return (
+    <div className="movies">
+      <MovieCard 
+        poster_path={ movie.poster_path }
+        title={ movie.title }
+      />
+    </div>
+    )
+  })
+  return <div className="movies-container">{movieCard}</div>
+}
+
+
+
+export default Movies
