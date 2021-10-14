762173f88a6d42766ab275de5a3ae6fd7636106d
Sarah <sarahrudy@gmail.com>
2021-10-14 17:48:38 -0600

    Add id in argument for getMovie method

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -3,7 +3,7 @@ import './MovieCard.css'
 
 const MovieCard = ({ id, poster_path, title, getMovie }) => {
   return (
-    <article className="movie-card" id={id} onClick={() => getMovie()}>
+    <article className="movie-card" id={id} onClick={() => getMovie(id)}>
       <img src={ poster_path } alt='movie poster'/>
       <p>{ title }</p>
     </article>
