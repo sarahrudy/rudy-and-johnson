91e50397a68df40ca1bb3aee6e5587f2b4a070b3
Sarah <sarahrudy@gmail.com>
2021-10-13 18:42:47 -0600

    Add contents to Movies component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import './App.css'
+import Movies from '../Movies/Movies'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       movies: movieData
+     
     }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <Movies />
+      </main>
+    )
   }
 }
 
---
+++ src/Movies/Movies.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Movies.css' 
+import { movieData } from './src/movieData.js'
+
+const Movies = () => {
+  <div>Hi!</div>
+}
+
+
+export default Movies
