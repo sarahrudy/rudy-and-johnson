cc87d386b35acf66061c50b395125db29a4e4474
Anna Johnson <ae.johnson2931@gmail.com>
2021-10-17 07:40:35 -0700

    Add hover styling to movie cards

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './MovieCard.css'
 
-const MovieCard = ({ id, poster_path, title, displayMovieDetails }) => {
+const MovieCard = ({ id, poster_path, title, average_rating, displayMovieDetails }) => {
   return (
     <article className="movie-card" id={id} onClick={() => displayMovieDetails(id)}>
       <img src={ poster_path } alt='movie poster'/>
       <p>{ title }</p>
+      <p>{ average_rating }</p>
     </article>
   )
 }
