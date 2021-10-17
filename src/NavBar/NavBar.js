256bef7764af996c12c3c6f91c60ab2211ea16c1
Anna Johnson <ae.johnson2931@gmail.com>
2021-10-17 07:35:47 -0700

    Add navbar styling

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,17 +4,20 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <div className="nav-bar">
-    <h1>RUDY JOHNSON</h1>
-    <form className="search-form">
-        <label>
-          <input
+      <h1 className="title">RUDY JOHNSON</h1>
+      <div className="search-container">
+        <p className="navBar-p">Your source for credible cinema reviews</p>
+        <form className="search-form">
+          <label className="form-label">
+            <input
             type="text"
             placeholder="Start your search here."
             name="searchTerm"
-          />
-        </label>
-        <button className="searchBtn">Submit</button>
-        </form>
+            />
+            </label>
+            <button className="searchBtn">Submit</button>
+          </form>
+        </div>
     </div>
   )
 }
