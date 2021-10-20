e6fe963fed19d63bd1420667d78abacd6e814dad
Sarah <sarahrudy@gmail.com>
2021-10-20 11:11:24 -0600

    Add back button to movie details page

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom'
 const MovieDetails = ({ movie }) => {
   return (
     <article className="movie-details" id={movie.id}>
+      <Link to="/" className="back-btn">Back</Link>
     <h3>{movie.title}</h3>
     <img src={movie.backdrop_path} alt=''/>
     <p>{movie.release_date}</p>
