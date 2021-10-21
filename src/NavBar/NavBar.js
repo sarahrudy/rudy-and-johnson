bfe8185601473702e8f7d421569eb4ee24616d16
Anna Johnson <ae.johnson2931@gmail.com>
2021-10-21 16:37:42 -0700

    Add functionality for clearing the search input as well as displaying an error message when no movies match query

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './NavBar.css'
 
-const NavBar = ({ submitSearch, handleChange }) => {
+const NavBar = ({ submitSearch, handleChange, value }) => {
 
   return (
     <div className="nav-bar">
       <h1 className="title">RUDY & JOHNSON</h1>
       <div className="search-container">
         <p className="navBar-p">Your source for credible cinema reviews</p>
+        <NavLink to="/about" className="about-link">About Us</NavLink>
         <form className="search-form">
           <label className="form-label">
             <input
             type="text"
+            value={value}
             placeholder="Start your search here."
             name="searchTerm"
             onChange={handleChange}
             />
             </label>
-            <button className="searchBtn" onClick={(event) => submitSearch(event)}>Submit</button>
+            <button className="searchBtn" onClick={(event) => submitSearch(event)} >Submit</button>
           </form>
         </div>
     </div>
