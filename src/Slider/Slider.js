15c2a95c117bf3a32d5cbf39b5bda29210f1756f
Anna Johnson <ae.johnson2931@gmail.com>
2021-10-23 14:21:40 -0700

    Refine styling for slider

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -15,31 +15,32 @@ class MovieSlider extends Component {
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 1000,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000
     };
     return (
       <div className="movie-slider">
-        <h2>Featured Films</h2>
         <Slider {...settings}>
           <div>
             <img className="slide-img"
             src={this.props.slideOne.backdrop_path}
             alt={this.props.slideOne.title} />
-            <h3>{this.props.slideOne.title}</h3>
+            <h3 className="slide-title"> {this.props.slideOne.title}</h3>
           </div>
           <div>
             <img className="slide-img"
             src={this.props.slideTwo.backdrop_path}
             alt={this.props.slideTwo.title} />
-            <h3>{this.props.slideTwo.title}</h3>
+            <h3 className="slide-title">{this.props.slideTwo.title}</h3>
           </div>
           <div>
             <img className="slide-img"
             src={this.props.slideThree.backdrop_path}
             alt={this.props.slideThree.title} />
-            <h3>{this.props.slideThree.title}</h3>
+            <h3 className="slide-title">{this.props.slideThree.title}</h3>
           </div>
         </Slider>
       </div>
