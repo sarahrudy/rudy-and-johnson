f12e096fd6952c93b8b292fe34586846b59f31f8
Sarah <sarahrudy@gmail.com>
2021-10-24 22:07:58 -0600

    Import ReactPlayer for trailer

--- src/Trailer/Trailer.js
+++ src/Trailer/Trailer.js
@@ -1,17 +1,24 @@
 import React from 'react'
+import ReactPlayer from 'react-player/youtube'
 import './Trailer.css' 
 
-const Trailer = ({ movieKey }) => {
+const Trailer = ({ url, trailerKey }) => {
   return (
-    <div className="video-responsive">
-      <iframe
-        key={movieKey}
+    <div className="react-player-wrapper">
+      {/* <ReactPlayer  
+        className='react-player'
+        url={`https://www.youtube.com/watch?v=${trailerKey}`}
+        width='100%'
+        height='100%'
+      /> */}
+      <iframe className="iframe"
+        key={trailerKey}
         width="853"
         height="480"
-        src={`https://www.youtube.com/embed/${movieKey}`}
+        src={`https://www.youtube.com/embed/${trailerKey}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        // allowFullScreen
+        allowFullScreen
         title="Embedded youtube"
       />
     </div>
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,10 +8,10 @@ export const getSingleMovie = (id) => {
     .then(response => response.json())
 }
 
-export const getTrailer = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
-    .then((response) => response.json());
-};
+export const getTrailer = (movie_id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movie_id}/videos`)
+    .then((response) => response.json())
+}
 
 export const checkError = (response) => {
   if (!response.ok) {
