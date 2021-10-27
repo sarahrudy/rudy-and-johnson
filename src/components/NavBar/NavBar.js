21db75a1f75dbf04c975465ba6ee26b7db7081dc
Sarah <sarahrudy@gmail.com>
2021-10-26 22:03:12 -0600

    Switch up styling for NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import whiteMovieReel from '../../images/white-movie-reel.png'
 import './NavBar.css'
 
 const NavBar = ({ submitSearch, handleChange, value }) => {
 
   return (
-    <div className="nav-bar">
-      <div className="title-container">
+    <section className="nav-bar-container">
+      <div>
         <NavLink to="/">
-        <h1 className="title">RUDY & JOHNSON</h1>
+          <h1 className="title"> 🎥 RUDY & JOHNSON</h1>
         </NavLink>
+          <p className='navBar-p'>Movie reviews you can trust</p>
+      </div>
+      <div className="about-us-container">
         <NavLink to="/about" className="about-link">ABOUT US</NavLink>
       </div>
       <div className="search-container">
-        <p className="navBar-p">Your source for credible cinema reviews</p>
         <form className="search-form">
-          <label className="form-label">
-            <input
+          <label>
+            <input className="form-input"
             type="text"
             value={value}
-            placeholder="Start your search here."
+            placeholder="Search movies here."
             name="searchTerm"
             onChange={handleChange}
             />
-            </label>
-            <button className="searchBtn" onClick={(event) => submitSearch(event)} >submit</button>
-          </form>
-        </div>
-    </div>
+          </label>
+          <button className="searchBtn" onClick={(event) => submitSearch(event)}>SUBMIT</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
